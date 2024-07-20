df682942a39e83dfafa812c1e9a5659ffa5aa561
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-07-19 22:54:10 -0600

    update project

---
+++ test_no_pom/first_open.py
@@ -0,0 +1,10 @@
+import time
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+time.sleep(3)
+
+driver.get("https://practicetestautomation.com/practice-test-login/")
+
+time.sleep(3)
