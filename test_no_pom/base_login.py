1766dddae19fa8bf95dcda76b888bae1205d0dfc
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-07-21 03:16:38 -0600

    update project

--- test_no_pom/base_login.py
+++ test_no_pom/base_login.py
@@ -9,8 +9,18 @@ driver.get("https://practicetestautomation.com/practice-test-login/")
 
 # Get locators
 
+# User name field
 username_locator = driver.find_element(By.ID, "username")
 
+# Password field
 password_locator = driver.find_element(By.NAME, "password")
 
+# Submit button
+
 submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+
+# Text from page after login
+text_locator = driver.find_element(By.TAG_NAME, "h1")
+
+# Button for log out
+log_out_button_locator = driver.find_element(By.LINK_TEXT, "Log out")
