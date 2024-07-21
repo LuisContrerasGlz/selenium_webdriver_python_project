68dc5f6c42e8a5c4bdc80a49633edbd85d29f9ec
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-07-21 03:23:36 -0600

    update project

--- test_no_pom/base_login.py
+++ test_no_pom/base_login.py
@@ -19,8 +19,22 @@ password_locator = driver.find_element(By.NAME, "password")
 
 submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
 
+
+# Steps for Loign
+
+username_locator.send_keys("student")
+
+password_locator.send_keys("Password123")
+
+submit_button_locator.click()
+
+# new page URL 
+
+actual_url = driver.current_url
+
 # Text from page after login
 text_locator = driver.find_element(By.TAG_NAME, "h1")
+actual_text = text_locator.text
 
 # Button for log out
 log_out_button_locator = driver.find_element(By.LINK_TEXT, "Log out")
