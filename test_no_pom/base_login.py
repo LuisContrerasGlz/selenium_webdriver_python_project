e3ee45c532723866f8411b406764bc0879076cb6
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-07-21 03:12:51 -0600

    update project

--- test_no_pom/base_login.py
+++ test_no_pom/base_login.py
@@ -1,7 +1,16 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 driver.get("https://practicetestautomation.com/practice-test-login/")
+
+# Get locators
+
+username_locator = driver.find_element(By.ID, "username")
+
+password_locator = driver.find_element(By.NAME, "password")
+
+submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
